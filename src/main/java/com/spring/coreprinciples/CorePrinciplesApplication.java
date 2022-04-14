bc724efc1b1f85fce5cabb75d7121dacfe58a1f9
Yoon <yoondev93@gmail.com>
2022-04-14 22:44:02 +0900

    객체 지향 설계와 스프링 - 스프링 부트 - 스프링 핵심 - 객체 지향 - 객체 지향 설계의 5가지 원칙 - SRP 단일 책임 원칙 - OCP 개방/폐쇄 원칙 - LSP 리스코프 치환 원칙 - ISP 인터페이스 분리 원칙 - DIP 의존관계 역전 원칙
    스프링 핵심 원리 이해1 - 예제 만들기
    - 프로젝트 생성

---
+++ src/main/java/com/spring/coreprinciples/CorePrinciplesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.coreprinciples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorePrinciplesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CorePrinciplesApplication.class, args);
+  }
+
+}
